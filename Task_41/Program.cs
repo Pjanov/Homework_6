0de77d102fcf32faa46766731bfa3fe7b35f1d72
Андрей <pjanov84@mail.ru>
2022-09-06 13:41:00 +0300

    Did task_41

--- Task_41/Program.cs
+++ Task_41/Program.cs
@@ -4,7 +4,38 @@
 0, 7, 8, -2, -2 -> 2
 1, -7, 567, 89, 223-> 3   */
 
+Console.WriteLine("ВВЕДИТЕ ПЯТЬ ЧИСЕЛ ! ");
+int[] array = new int[5]; // М числа
 
+void EnterNumber(int[] array) // запрашивает ввод числа у пользователя
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите {i + 1} число: ");
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
 
+    Console.WriteLine(); // пустая строка
 
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}{(i != array.Length - 1 ? ", " : "")}");
+    }
+}
 
+void PositiveNumber(int[] array) // считает положительные числа
+{
+    int even = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            even++;
+        }
+    }
+    Console.Write($" -> {even}"); 
+}
+
+EnterNumber(array); 
+PositiveNumber(array);  
--- Task_43/Program.cs
+++ Task_43/Program.cs
@@ -4,9 +4,19 @@ y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задают
 
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)        */
 
+void EnterNumber(double pointB1, double pointK1, double pointB2, double pointK2) // найдёт точку пересечения двух прямых
+{
+    
+    double b1 = pointB1;
+    double k1 = pointK1;
+    double b2 = pointB2;
+    double k2 = pointK2;
 
+    double x = (b2 - b1) / (k1 - k2);
+    double y1 = k1 * x + b1;
+    double y2 = k2 * x + b2;
 
+    Console.Write($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({y1}; {y2})");
+}
 
-
-
-
+EnterNumber(2, 5, 4, 9);
